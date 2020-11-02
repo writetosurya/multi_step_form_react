a1fbeb0c897fdff726406518d68cd05c1cdab1f4
writetosurya <62762212+writetosurya@users.noreply.github.com>
2020-11-02 09:53:17 +0530

    Update FormUserDetails.js

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
@@ -11,40 +13,63 @@ export class FormUserDetails extends Component {
     }
 
     render() {
-        const { values, handleChange } = this.props;
-        return (
-            <MuiThemeProvider>
-                <React.Fragment>
-                    <AppBar title="Multi Step User Form Deatils" />
-                    <TextField
-                        hintText="Enter Your First Name"
-                        floatingLabelText="First Name"
-                        onChange={handleChange('firstName')}
-                        defaultValue={values.firstName}
-                    />
-                    <br />
-                    <TextField
-                        hintText="Enter Your Last Name"
-                        floatingLabelText="Last Name"
-                        onChange={handleChange('lastName')}
-                        defaultValue={values.lastName}
-                    />
-                    <br />
-                    <TextField
-                        hintText="Enter Your Email"
-                        floatingLabelText="Email"
-                        onChange={handleChange('email')}
-                        defaultValue={values.email}
-                    />
-                    <br />
-                    <RaisedButton
-                        label="Continue"
-                        primary={true}
-                        style={styles.button}
-                        onClick={this.continue}
-                    />
-                </React.Fragment>
-            </MuiThemeProvider>
+        const {
+            values,
+            handleChange
+        } = this.props;
+        return ( <
+            MuiThemeProvider >
+            <
+            React.Fragment >
+            <
+            AppBar title = "Multi Step User Form Deatils" / >
+            <
+            TextField hintText = "Enter Your First Name"
+            floatingLabelText = "First Name"
+            onChange = {
+                handleChange('firstName')
+            }
+            defaultValue = {
+                values.firstName
+            }
+            /> <
+            br / >
+            <
+            TextField hintText = "Enter Your Last Name"
+            floatingLabelText = "Last Name"
+            onChange = {
+                handleChange('lastName')
+            }
+            defaultValue = {
+                values.lastName
+            }
+            /> <
+            br / >
+            <
+            TextField hintText = "Enter Your Email"
+            floatingLabelText = "Email"
+            onChange = {
+                handleChange('email')
+            }
+            defaultValue = {
+                values.email
+            }
+            /> <
+            br / >
+            <
+            RaisedButton label = "Continue"
+            primary = {
+                true
+            }
+            style = {
+                styles.button
+            }
+            onClick = {
+                this.continue
+            }
+            /> <
+            /React.Fragment> <
+            /MuiThemeProvider>
         )
     }
 }
