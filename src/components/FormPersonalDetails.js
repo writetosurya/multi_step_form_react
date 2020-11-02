dbef6e94dc024045525e6d792f004b90ecc8a2fa
writetosurya <62762212+writetosurya@users.noreply.github.com>
2020-11-02 09:52:23 +0530

    Update FormPersonalDetails.js

--- src/components/FormPersonalDetails.js
+++ src/components/FormPersonalDetails.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, {
+    Component
+} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
@@ -16,46 +18,74 @@ export class FormPersonalDetails extends Component {
     }
 
     render() {
-        const { values, handleChange } = this.props;
-        return (
-            <MuiThemeProvider>
-                <React.Fragment>
-                    <AppBar title="Enter Personal Details" />
-                    <TextField
-                        hintText="Enter Your Occupation"
-                        floatingLabelText="Occupation"
-                        onChange={handleChange('occupation')}
-                        defaultValue={values.occupation}
-                    />
-                    <br />
-                    <TextField
-                        hintText="Enter Your City"
-                        floatingLabelText="City"
-                        onChange={handleChange('city')}
-                        defaultValue={values.city}
-                    />
-                    <br />
-                    <TextField
-                        hintText="Enter Your Bio"
-                        floatingLabelText="Bio"
-                        onChange={handleChange('bio')}
-                        defaultValue={values.bio}
-                    />
-                    <br />
-                    <RaisedButton
-                        label="Continue"
-                        primary={true}
-                        style={styles.button}
-                        onClick={this.continue}
-                    />
-                    <RaisedButton
-                        label="Back"
-                        primary={false}
-                        style={styles.button}
-                        onClick={this.back}
-                    />
-                </React.Fragment>
-            </MuiThemeProvider>
+        const {
+            values,
+            handleChange
+        } = this.props;
+        return ( <
+            MuiThemeProvider >
+            <
+            React.Fragment >
+            <
+            AppBar title = "Enter Personal Details" / >
+            <
+            TextField hintText = "Enter Your Occupation"
+            floatingLabelText = "Occupation"
+            onChange = {
+                handleChange('occupation')
+            }
+            defaultValue = {
+                values.occupation
+            }
+            /> <
+            br / >
+            <
+            TextField hintText = "Enter Your City"
+            floatingLabelText = "City"
+            onChange = {
+                handleChange('city')
+            }
+            defaultValue = {
+                values.city
+            }
+            /> <
+            br / >
+            <
+            TextField hintText = "Enter Your Bio"
+            floatingLabelText = "Bio"
+            onChange = {
+                handleChange('bio')
+            }
+            defaultValue = {
+                values.bio
+            }
+            /> <
+            br / >
+            <
+            RaisedButton label = "Continue"
+            primary = {
+                true
+            }
+            style = {
+                styles.button
+            }
+            onClick = {
+                this.continue
+            }
+            /> <
+            RaisedButton label = "Back"
+            primary = {
+                false
+            }
+            style = {
+                styles.button
+            }
+            onClick = {
+                this.back
+            }
+            /> <
+            /React.Fragment> <
+            /MuiThemeProvider>
         )
     }
 }
